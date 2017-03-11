0d1b298f6bd9aa564174cd8ac829fb06375aaab3
RiyaMRoy04 <mary.riya@gmail.com>
2017-03-11 10:18:57 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,41 @@
+// Example 3: maintains state.
+
+var prompt = require('prompt-sync')();
+var ConversationV1 = require('watson-developer-cloud/conversation/v1');
+
+// Set up Conversation service.
+var conversation = new ConversationV1({
+  username: 'a054c82a-3415-4bb6-aab2-6ff567ae8a69', // replace with username from service key
+  password: 'B1DGAjZyIycL', // replace with password from service key
+  path: { workspace_id: '654537ba-f22a-4db0-9a55-cc32bca8e9c5' }, // replace with workspace ID
+  version_date: '2016-07-11'
+});
+
+// Start conversation with empty message.
+conversation.message({}, processResponse);
+
+// Process the conversation response.
+function processResponse(err, response) {
+  if (err) {
+    console.error(err); // something went wrong
+    return;
+  }
+
+  // If an intent was detected, log it out to the console.
+  if (response.intents.length > 0) {
+    console.log('Detected intent: #' + response.intents[0].intent);
+  }
+  
+  // Display the output from dialog, if any.
+  if (response.output.text.length != 0) {
+      console.log(response.output.text[0]);
+  }
+
+  // Prompt for the next round of input.
+    var newMessageFromUser = prompt('>> ');
+    // Send back the context to maintain state.
+    conversation.message({
+      input: { text: newMessageFromUser },
+      context : response.context,
+    }, processResponse)
+}
